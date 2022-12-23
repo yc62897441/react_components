110873eaa39247e72fc5d300dfee9e2b645b4a63
SHINFOX\fp0023 <yc62897441@gmail.com>
2022-12-23 12:18:47 +0800

    feat: MobileNavOverly

---
+++ src/contains/HeaderOverly.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.header`
+    position: relative;
+    top: 0px;
+    width: 100vw;
+    height: 60px;
+    padding: 10px 0;
+    z-index: 999;
+`
+
+const MobileNavOverly = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: -9997;
+    background: rgba(1, 2, 2, 0.6);
+    display: ${({ isToggleOn }) => (isToggleOn ? 'block' : 'none')};
+    transition: ease-in-out 0.2s;
+    @media (min-width: 992px) {
+        display: none;
+    }
+`
+
+const Div = styled.div`
+    z-index: 9998;
+`
+
+const Button = styled.button`
+    z-index: 9998;
+`
+
+function HeaderOverly() {
+    const [isToggleOn, setIsToggleOn] = useState(false)
+
+    // 手機版尺寸時，點選  <NavToggleBarWrapper /> 顯示 <MobileNavOverly>
+    function handleToggle() {
+        console.log('asdas')
+        setIsToggleOn((n) => !n)
+    }
+
+    // 監聽使用者視窗大小改變，切換成電腦版時，隱藏 <MobileNavOverly>
+    function handleRWD() {
+        if (window.innerWidth > 992) {
+            setIsToggleOn(false)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleRWD)
+        if (window.innerWidth > 992) {
+            setIsToggleOn(false)
+        }
+        return () => {
+            window.removeEventListener('resize', handleRWD)
+        }
+    }, [])
+
+    window.removeEventListener('resize', handleRWD)
+
+    return (
+        <>
+            <HeaderWrapper>
+                <Div>
+                    <Button onClick={() => handleToggle()}>Show modal(at the same while triggering MobileNavOverly)</Button>
+                </Div>
+                <MobileNavOverly isToggleOn={isToggleOn} />
+            </HeaderWrapper>
+        </>
+    )
+}
+
+export default HeaderOverly

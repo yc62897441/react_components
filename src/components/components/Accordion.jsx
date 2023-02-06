e109fb64016f831e1ec0e8cb831c947e5f86d818
SHINFOX\fp0023 <yc62897441@gmail.com>
2023-02-06 12:15:20 +0800

    test

--- src/components/components/Accordion.jsx
+++ src/components/components/Accordion.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'webpack'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const AccordionsWrapper = styled.div`
@@ -13,25 +13,39 @@ const AccordionsWrapper = styled.div`
 const AccordionWrapper = styled.div`
     width: 500px;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
-    border-radius: 15px;
 `
 
 const AccordionContainer = styled.div`
+    position: relative;
     width: 100%;
-    padding: 5px 15px;
 `
 
 const AccordionTitleContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
+    padding: 15px 15px;
+    border: ${({ isActive }) => (isActive ? '1px solid rgba(255, 219, 172, 1)' : '1px solid transparent)')};
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+    cursor: pointer;
+
+    div {
+        transform: ${({ isActive }) => (isActive ? 'rotate(180deg)' : 'rotate(0deg)')};
+        transition: all 0.3s;
+    }
+    transition: all 0.3s;
 `
 
 const AccordionContentContainer = styled.div`
-    /* position: absolute; */
+    position: ${({ isShow }) => (isShow ? 'static' : 'absolute')};
     width: 100%;
-    /* opacity: ${({ isShow }) => (isShow ? '1' : '0')}; */
-    /* transform: ${({ isShow }) => (isShow ? 'scale(1, 1)' : 'scale(0, 1)')}; */
-    transition: all 0.2s;
+    padding: 15px 15px;
+    transform: ${({ isShow }) => (isShow ? 'scale(1, 1)' : 'scale(1, 0)')};
+    transform-origin: top;
+    opacity: ${({ isShow }) => (isShow ? '1' : '0')};
+    transition: all 0.3s;
 `
 
 const dummyData = [
@@ -68,13 +82,21 @@ function Accordion() {
             {dummyData.length > 0 && (
                 <AccordionWrapper>
                     {dummyData.map((item) => {
+                        const isActive = showId === item.id ? true : false
                         const isShow = showId === item.id ? true : false
                         return (
                             <AccordionContainer key={item.id}>
-                                <AccordionTitleContainer onClick={() => handleShow(item.id)}>
+                                <AccordionTitleContainer isActive={isActive} onClick={() => handleShow(item.id)}>
                                     <h1>{item.title}</h1>
+                                    <div>
+                                        <i class="fa fa-arrow-down" aria-hidden="true"></i>
+                                    </div>
                                 </AccordionTitleContainer>
-                                <AccordionContentContainer isShow={isShow}>{item.content}</AccordionContentContainer>
+                                <AccordionContentContainer isShow={isShow}>
+                                    <h1>{item.content}</h1>
+                                    <h1>{item.content}</h1>
+                                    <h1>{item.content}</h1>
+                                </AccordionContentContainer>
                             </AccordionContainer>
                         )
                     })}
